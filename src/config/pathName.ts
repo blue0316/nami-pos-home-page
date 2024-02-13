76b61024ee6121fddf6bd9e9d6b5325027238df2
blue0316 <greatblueknight@gmail.com>
2024-02-13 12:51:23 +0900

    feat: locale path names

--- src/config/pathName.ts
+++ src/config/pathName.ts
@@ -3,13 +3,25 @@ import { locales } from '@/i18n';
 
 export const pathnames = {
   '/': '/',
-  '/home': {
-    en: '/home',
-    ja: '/ホーム',
-  },
-  '/about': {
-    en: '/about',
+  '/ourProduct': {
+    en: '/ourProduct',
     ja: '/およそ',
+  },
+  '/equipments': {
+    en: '/equipments',
+    ja: '/周辺機器',
+  },
+  '/pricing': {
+    en: '/pricing',
+    ja: '/料金プラン',
+  },
+  '/contactUs': {
+    en: '/contactUs',
+    ja: '/お問い合わせ',
+  },
+  '/storeLogin': {
+    en: '/storeLogin',
+    ja: '/店舗ログイン',
   },
 } satisfies Pathnames<typeof locales>;
 
